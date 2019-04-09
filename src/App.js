51c4e851c21dbd178f43e4269cb0393357780d43
Jean Carrillo <jeancarrillo@gmail.com>
2019-04-09 23:24:48 +0200

    json test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import './App.css';
-import * as Labyrinths from './labyrinths.json';
-import * as Items from './items.json';
+// import * as Labyrinths from './labyrinths.json';
+// import * as Items from './items.json';
 // TO DO : using data.json instead of labyrinths and items
-// import * as Data from './data.json';
+import * as Data from './data.json';
 import Game from './Game';
 
 const App = () => {
-  const labyrinth = Labyrinths.labyrinth1;
-  const items = Items.labyrinth1_items;
+  const selectedLevel = Data.levels['001'];
+  //  const labyrinth = Labyrinths.labyrinth1;
+  //  const items = Items.labyrinth1_items;
   return (
     // TO DO : choose the level
     <div>
       <Game
-        labyrinth={labyrinth}
-        items={items}
+        labyrinth={selectedLevel}
         count="120"
-        playerX="0"
-        playerY="0"
       />
     </div>
   );
