bd06ea0cebbdc5e1b9a310e38c5110b5ed73cbc1
Justine Vareille <vareille.justine@gmail.com>
2019-04-10 14:18:52 +0200

    ajout ronflex chrono

--- src/Chrono.js
+++ src/Chrono.js
@@ -1,46 +1,46 @@
 import React, { Component } from 'react';
 
 class Chrono extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0,
-            running: false,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      running: false,
     }
+  }
 
-    componentWillMount() {
-        this.timer = setInterval(() => {
-            const newCount = this.state.count - 1;
-            this.setState({
-                count: newCount >= 0 ? newCount : 0,
-                running: true,
-            });
-        }, 1000);
-    }
+  componentWillMount() {
+    this.timer = setInterval(() => {
+      const newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0,
+        running: true,
+      });
+    }, 1000);
+  }
 
-    componentDidMount() {
-        this.setState({ count: this.props.count })
-    }
+  componentDidMount() {
+    this.setState({ count: this.props.count })
+  }
 
-    format(time) {
-        let seconds = time % 60;
-        let minutes = Math.floor(time / 60);
-        minutes = minutes.toString().length === 1 ? "0" + minutes : minutes;
-        seconds = seconds.toString().length === 1 ? "0" + seconds : seconds;
-        return minutes + ':' + seconds;
-    }
+  format(time) {
+    let seconds = time % 60;
+    let minutes = Math.floor(time / 60);
+    minutes = minutes.toString().length === 1 ? "0" + minutes : minutes;
+    seconds = seconds.toString().length === 1 ? "0" + seconds : seconds;
+    return minutes + ':' + seconds;
+  }
 
 
 
-    render() {
-        const { count } = this.state;
-        return (
-            <div>
-                <h1>{this.format(count)}</h1>
-            </div>
+  render() {
+    const { count } = this.state;
+    return (
+      <div className='Timer'>
+        <h1>{this.format(count)}</h1>
+      </div>
 
-        );
-    }
+    );
+  }
 }
 export default Chrono
