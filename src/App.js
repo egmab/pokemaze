6b33624fbc3b973c1e1188a3ac480b40452b2845
eugenie <Vous@exemple.com>
2019-04-12 10:27:15 +0200

    router home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 import SoloGame from './SoloGame';
+import Home from './Home';
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Switch>
-        <Route exact path="/" component={SoloGame} />
+        <Route exact path="/" component={Home} />
+        <Route path="/solo-game" component={SoloGame} />
       </Switch>
     </div>
   </BrowserRouter>
