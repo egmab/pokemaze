d054859060ab2adf415c1ef4a5afd957c7b1b993
eugenie <Vous@exemple.com>
2019-04-12 11:36:50 +0200

    router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 import SoloGame from './SoloGame';
+import SoloGame2 from './SoloGame2';
 import Home from './Home';
 
 const App = () => (
@@ -9,7 +10,8 @@ const App = () => (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/solo-game" component={SoloGame} />
+        <Route path="/solo-game-1" component={SoloGame} />
+        <Route path="/solo-game-2" component={SoloGame2} />
       </Switch>
     </div>
   </BrowserRouter>
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
   return (
-    <div>
-      hello
+    <div className="home">
+      <button
+        className="largeBtn2"
+        type="button"
+        size="lg"
+      >
+        <Link to="/solo-game-1">Play solo level</Link>
+
+      </button>
+      <button
+        className="largeBtn2"
+        type="button"
+        size="lg"
+      >
+        <Link to="/solo-game-2">Demo chrono</Link>
+      </button>
     </div>
   );
 };
