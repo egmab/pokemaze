c662a52a2241339b0e6e92f4db498eecab1acb3d
eugenie <Vous@exemple.com>
2019-04-16 09:15:45 +0200

    component KeyBars

---
+++ src/KeysBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class KeysBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  
+
+  render() {
+
+    return (
+ 
+
+    );
+  }
+}
+export default KeysBar;
