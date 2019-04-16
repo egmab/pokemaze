d782ccb15213230a984a6610753703aca2859140
eugenie <Vous@exemple.com>
2019-04-16 18:00:23 +0200

    jauge qui évolue en fonction du nombre de keys attrapées

--- src/KeysBar.js
+++ src/KeysBar.js
@@ -1,22 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
+import KeyElement from './KeyElement';
+import './KeysBar.css';
 
 
-class KeysBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
 
-    };
-  }
 
+const KeysBar = ({ collectedKeys, numberOfKeys, typeOfKey, finalDoorID }) => {
+  const tab = new Array(numberOfKeys).fill(0);
+  const style = {
+    opacity: 1,
+    display: 'inline',
+    width: '40%',
+    marginTop: '30%',
+  };
+  const styleDoor = {
+    backgroundImage: `url(${`./assets/items/${finalDoorID}.png`})`,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '100%,contain',
+    opacity: 0.6,
+  };
+  if (collectedKeys === numberOfKeys) {
+    styleDoor.opacity = 1;
+    style.display = 'none';
+
+  };
   
 
-  render() {
+  return (
+    <div className="keysBar">
+      {tab.map((number, index) => {
+        return (
+          <div className="keysContainer">
+            <KeyElement isCaught={collectedKeys === index + 1} typeOfKey={typeOfKey} />
+          </div>
+        );
+      })}
+      <div className="bigKeyCircle" style={{ ...styleDoor }}>
+        <img src="./assets/capacities/cadenas.png" style={{ ...style }} alt="Final door" />
+      </div>
+    </div>
 
-    return (
- 
-
-    );
-  }
-}
+  );
+};
 export default KeysBar;
