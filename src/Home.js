2f05ea2b5762ce2dfba8c63f65d2cd4e5ffa6ad3
Jean Carrillo <jeancarrillo@gmail.com>
2019-04-17 10:16:54 +0200

    local storage prototype

--- src/Home.js
+++ src/Home.js
@@ -4,33 +4,56 @@ import './Home.css';
 
 
 class Home extends Component {
-
   constructor(props) {
     super(props);
+    this.data = {
+      pokemons: [1, 2, 3],
+      badges: [23, 13],
+      titre: 'je suis très très fort',
+    };
     this.state = {
       user: '',
-    }
+    };
     this.onChange = this.onChange.bind(this);
   }
 
 
   onChange(event) {
-    localStorage.setItem('player', event.target.value);
     this.setState({
-      user: event.target.value
+      user: event.target.value,
     });
   }
 
+  onSubmit = (event) => {
+    event.preventDefault();
+    const { user } = this.state;
+    const player = user;
+    this.connectedUser = user;
+    if (localStorage.getItem(player)) {
+      alert('utilisateur déjà existant');
+    } else {
+      localStorage.setItem(player, JSON.stringify(this.data));
+      console.log(JSON.parse(localStorage.getItem('data')));
+    }
+    this.setState({ user: '' });
+  }
+
   render() {
+    const { user } = this.state;
     return (
       <div className="home">
         <img src="./assets/logopokemaze.png" alt="logo" />
         <div className="form">
-          <input
-            type="text"
-            id="title"
-            name="title"
-          />
+          <form onSubmit={this.onSubmit}>
+            <input
+              onChange={this.onChange}
+              type="text"
+              id="title"
+              name="title"
+              value={user}
+            />
+            <input type="submit" value="Connect" style={{ display: 'none' }} />
+          </form>
         </div>
         <div className="buttonContainerHome">
           <Link to="/solo-game-1">
@@ -41,13 +64,12 @@ class Home extends Component {
               style={{ marginRight: 100 }}
             >
               Solo mode
-        </button>
+            </button>
           </Link>
         </div>
       </div>
     );
   }
-
 }
 
 export default Home;
