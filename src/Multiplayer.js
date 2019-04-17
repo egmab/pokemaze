f51e00be5ca50e1d083d8d530f150f5c98a152ba
Jean Carrillo <jeancarrillo@gmail.com>
2019-04-17 22:06:30 +0200

    gameData.json is added to local storage in App.js, restructuration of gameData.json, small fixes here and there

--- src/Multiplayer.js
+++ src/Multiplayer.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import * as GameData from './gameData.json';
 import MultiplayerGame from './MultiplayerGame';
 
 const Multiplayer = () => {
   const player1 = 'Player 1';
   const player2 = 'Player 2';
-  const selectedLevel = GameData.levels.multi001;
+  const selection = '001';
+  const selectedLevel = JSON.parse(localStorage.getItem('GameData')).default.levels.multiplayer[selection];
   return (
     <div>
       <MultiplayerGame level={selectedLevel} player1={player1} player2={player2} />
