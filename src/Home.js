02b9bb6a4572633a7ec77f90093e72aa1f5b557e
Justine Vareille <vareille.justine@gmail.com>
2019-04-17 09:17:25 +0200

    local storage

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
 
-const Home = () => (
-  <div className="home">
-    <img src="./assets/logopokemaze.png" alt="logo" />
-    <div className="form">
-      <input
-        type="text"
-        id="title"
-        name="title"
-      />
-    </div>
-    <div className="buttonContainerHome">
-      <Link to="/solo-game-1">
-        <button
-          className="homeButton"
-          type="button"
-          size="lg"
-          style={{ marginRight: 100 }}
-        >
-          Solo mode
+class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+    }
+    this.onChange = this.onChange.bind(this);
+  }
+
+
+  onChange(event) {
+    localStorage.setItem('player', event.target.value);
+    this.setState({
+      user: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <img src="./assets/logopokemaze.png" alt="logo" />
+        <div className="form">
+          <input
+            type="text"
+            id="title"
+            name="title"
+          />
+        </div>
+        <div className="buttonContainerHome">
+          <Link to="/solo-game-1">
+            <button
+              className="homeButton"
+              type="button"
+              size="lg"
+              style={{ marginRight: 100 }}
+            >
+              Solo mode
         </button>
-      </Link>
-    </div>
-  </div>
-);
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+}
 
 export default Home;
