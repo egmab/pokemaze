bb47f403b5d82c68182b561166ffb6e6e9f1b3b4
eugenie <Vous@exemple.com>
2019-04-17 15:44:10 +0200

    bouton back to menu et refresh du niveau

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import SoloGame from './SoloGame';
 import SoloGame2 from './SoloGame2';
 import Home from './Home';
 
+// <Route path="/game" render={props => <Game level={selectedLevel} {...props} />} />
+
+
 const App = () => (
   <BrowserRouter>
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/solo-game-1" component={SoloGame} />
+        <Route path="/solo-game" component={SoloGame} />
         <Route path="/solo-game-2" component={SoloGame2} />
       </Switch>
     </div>
