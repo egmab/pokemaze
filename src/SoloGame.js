9b7397994709308d6dd48bc8da77c0faa4d3e6b8
Jean Carrillo <jeancarrillo@gmail.com>
2019-04-21 22:17:10 +0200

    added level selection, should work soon with custom levels

--- src/SoloGame.js
+++ src/SoloGame.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Game from './Game';
 
-const SoloGame = () => {
-  // TO DO : choose the level
-  const selection = '001';
-  const selectedLevel = JSON.parse(localStorage.getItem('GameData')).default.levels.solo[selection];
-  return (
-    <div>
-      <Game level={selectedLevel} />
-    </div>
-  );
-};
+class SoloGame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectionDone: false,
+    };
+  }
+
+  selectLevel = (event) => {
+    this.selectedLevel = JSON.parse(localStorage.getItem('GameData')).default.levels.solo[event.target.value];
+    this.setState({ selectionDone: true });
+  }
+
+  render() {
+    const { selectionDone } = this.state;
+    const soloLevels = Object.getOwnPropertyNames(JSON.parse(localStorage.getItem('GameData')).default.levels.solo);
+    const customLevels = Object.getOwnPropertyNames(JSON.parse(localStorage.getItem('GameData')).default.levels.customLevels);
+    return (
+      <div className="home">
+        {
+          selectionDone
+            ? <Game level={this.selectedLevel} />
+            : soloLevels.map((level, index) => (
+              <button className="homeButton" type="button" key={`levelId-${index + 1}`} value={level} onClick={this.selectLevel}>
+                {level}
+              </button>
+            ))
+        }
+        {
+          selectionDone
+            ? null
+            : customLevels.map((level, index) => (
+              <button className="homeButton" type="button" key={`customLevelId-${index + 1}`} value={level} onClick={this.selectLevel}>
+                {level}
+              </button>
+            ))
+        }
+      </div>
+    );
+  }
+}
 
 export default SoloGame;
