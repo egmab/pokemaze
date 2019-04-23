4a191419445ac7e06004bef8bf07eaa5f02c3097
Justine Vareille <vareille.justine@gmail.com>
2019-04-23 10:45:03 +0200

    page connection

--- src/Home.js
+++ src/Home.js
@@ -1,55 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
 
-const Home = () => (
-  <div className="home">
-    <img src="./assets/logopokemaze.png" alt="logo" />
-    <div className="buttonContainerHome">
-      <Link to="/solo-game">
-        <button
-          className="homeButton"
-          type="button"
-          size="lg"
-          style={{ marginRight: 100 }}
-        >
-          Play solo
-        </button>
-      </Link>
-      <Link to="/pokedex">
-        <button
-          className="homeButton"
-          type="button"
-          size="lg"
-          style={{ marginRight: 100 }}
-        >
-          Pokedex
-        </button>
-      </Link>
-      <Link to="/multiplayer">
-        <button
-          className="homeButton"
-          type="button"
-          size="lg"
-          style={{ marginRight: 100 }}
-        >
-          Multiplayer
-        </button>
-      </Link>
-      <Link to="/pokeditor">
-        <button
-          className="homeButton"
-          type="button"
-          size="lg"
-          style={{ marginRight: 100 }}
-        >
-          Pokeditor
-        </button>
-      </Link>
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.data = {
+      pokemons: [1, 2, 3],
+      badges: [23, 13],
+      titre: 'je suis très très fort',
+    };
+    this.state = {
+      user: '',
+    };
+    this.onChange = this.onChange.bind(this);
+  }
 
-    </div>
-  </div>
-);
+
+  onChange(event) {
+    this.setState({
+      user: event.target.value,
+    });
+  }
+
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const { user } = this.state;
+    const player = user;
+    this.connectedUser = user;
+    if (localStorage.getItem(player)) {
+      alert('utilisateur déjà existant');
+    } else {
+      localStorage.setItem(player, JSON.stringify(this.data));
+      console.log(JSON.parse(localStorage.getItem('data')));
+    }
+    this.setState({ user: '' });
+  }
+
+  render() {
+    const { user } = this.state;
+    return (
+      <div className="home">
+        <img src="./assets/logopokemaze.png" alt="logo" />
+        <div className="buttonContainerHome">
+          <Link to="/solo-game">
+            <button
+              className="homeButton"
+              type="button"
+              size="lg"
+              style={{ marginRight: 100 }}
+            >
+              Play solo
+            </button>
+          </Link>
+          <Link to="/pokedex">
+            <button
+              className="homeButton"
+              type="button"
+              size="lg"
+              style={{ marginRight: 100 }}
+            >
+              Pokedex
+            </button>
+          </Link>
+          <Link to="/multiplayer">
+            <button
+              className="homeButton"
+              type="button"
+              size="lg"
+              style={{ marginRight: 100 }}
+            >
+              Multiplayer
+            </button>
+          </Link>
+          <Link to="/pokeditor">
+            <button
+              className="homeButton"
+              type="button"
+              size="lg"
+              style={{ marginRight: 100 }}
+            >
+              Pokeditor
+            </button>
+          </Link>
+
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;
