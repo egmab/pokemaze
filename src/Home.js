c2bedc314eada83cdfc367146259a8cd1551bb1e
Justine Vareille <vareille.justine@gmail.com>
2019-04-24 15:05:36 +0200

    ajout bouton deconnection

--- src/Home.js
+++ src/Home.js
@@ -17,6 +17,15 @@ class Home extends Component {
     this.onChange = this.onChange.bind(this);
   }
 
+  componentWillMount() {
+    if (localStorage.getItem('connectedPlayer')) {
+      this.setState({ userOne: JSON.parse(localStorage.getItem('connectedPlayer')), playerOneConnected: true });
+    }
+    if (localStorage.getItem('connectedPlayer2')) {
+      this.setState({ userTwo: JSON.parse(localStorage.getItem('connectedPlayer2')), playerTwoConnected: true });
+    }
+  }
+
   onChange(event) {
     const user = event.target.id;
     this.setState({
@@ -37,6 +46,18 @@ class Home extends Component {
     } else {
       localStorage.setItem('connectedPlayer', JSON.stringify(userOne));
     }
+  }
+
+  onDisconnectOne = (event) => {
+    event.preventDefault();
+    const { playerOneConnected } = this.state;
+    this.setState({ playerOneConnected: !playerOneConnected });
+  }
+
+  onDisconnectTwo = (event) => {
+    event.preventDefault();
+    const { playerTwoConnected } = this.state;
+    this.setState({ playerTwoConnected: !playerTwoConnected });
   }
 
 
@@ -71,6 +92,14 @@ class Home extends Component {
                   Player 1 :
                   {userOne}
                 </h3>
+                <form onSubmit={this.onDisconnectOne}>
+                  <input
+                    className="connectButton"
+                    size="lg"
+                    type="submit"
+                    value="Disconnect"
+                  />
+                </form>
               </div>
             )
             : (
@@ -105,6 +134,14 @@ class Home extends Component {
                   Player 2 :
                   {userTwo}
                 </h3>
+                <form onSubmit={this.onDisconnectTwo}>
+                  <input
+                    className="connectButton"
+                    size="lg"
+                    type="submit"
+                    value="Disconnect"
+                  />
+                </form>
               </div>
             )
             : (
@@ -145,26 +182,7 @@ class Home extends Component {
               Play solo
             </button>
           </Link>
-          <Link to="/pokedex">
-            <button
-              className="homeButton"
-              type="button"
-              size="lg"
-              style={{ marginRight: 100 }}
-            >
-              Pokedex
-            </button>
-          </Link>
-          <Link to="/multiplayer">
-            <button
-              className="homeButton"
-              type="button"
-              size="lg"
-              style={{ marginRight: 100 }}
-            >
-              Multiplayer
-            </button>
-          </Link>
+
           <Link to="/pokeditor">
             <button
               className="homeButton"
@@ -176,8 +194,31 @@ class Home extends Component {
             </button>
           </Link>
 
+          <Link to="/pokedex">
+            <button
+              className="homeButton"
+              type="button"
+              size="lg"
+              style={{ marginRight: 100 }}
+            >
+              Pokedex
+            </button>
+          </Link>
+
+          <Link to="/multiplayer">
+            <button
+              className="homeButton"
+              type="button"
+              size="lg"
+              style={{ marginRight: 100 }}
+            >
+              Multiplayer
+            </button>
+          </Link>
+
+
         </div>
-      </div >
+      </div>
     );
   }
 }
