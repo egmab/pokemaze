2f65f06197825142a331be38e6adf038d86734dc
Justine Vareille <vareille.justine@gmail.com>
2019-05-01 13:01:37 +0200

    starter 3 2 1 GO

--- src/Starter.js
+++ src/Starter.js
@@ -4,14 +4,58 @@ class Starter extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      time: {},
+      seconds: 3,
+    };
+    this.timer = 0;
+    this.countDown = this.countDown.bind(this);
+  }
 
+
+  componentDidMount() {
+    const { seconds } = this.state;
+    const timeLeft = this.secondsToTime(seconds);
+    this.setState({ time: timeLeft });
+  }
+
+  secondsToTime(secs) {
+    const hours = Math.floor(this.secs / (60 * 60));
+
+    const divisorForMinutes = secs % (60 * 60);
+    const minutes = Math.floor(divisorForMinutes / 60);
+
+    const divisorForSeconds = divisorForMinutes % 60;
+    const seconds = Math.ceil(divisorForSeconds);
+
+    let obj = {
+      "h": hours,
+      "m": minutes,
+      "s": seconds
+    };
+
+    return obj;
+  }
+
+  countDown() {
+    const seconds = this.state.seconds - 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
+    });
+
+    if (seconds === 0) {
+      clearInterval(this.timer);
     }
   }
 
   render() {
+    const { time } = this.state;
     return (
       <div>
-
+        m:
+        {time.m}
+        s:
+        {time.s}
       </div>
     );
   }
