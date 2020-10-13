de4ec7387cf04729d59aff444e85492456e8af25
eugenie <eugenie.margulis@gmail.com>
2020-10-13 20:46:52 +0200

    Updates

---
+++ precache-manifest.0c1d6d337a9d07c2fa0cc12dbe51743c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c5727ed45efa793e776",
+    "url": "/pokemaze/static/css/main.cf047301.chunk.css"
+  },
+  {
+    "revision": "4c5727ed45efa793e776",
+    "url": "/pokemaze/static/js/main.2f9bc675.chunk.js"
+  },
+  {
+    "revision": "610d3204289270f97f8d",
+    "url": "/pokemaze/static/js/runtime~main.eb1e53e9.js"
+  },
+  {
+    "revision": "3d2be86a2290b26b96e5",
+    "url": "/pokemaze/static/js/2.5046fa77.chunk.js"
+  },
+  {
+    "revision": "78a721ac85f804de5de5d960bd3d976c",
+    "url": "/pokemaze/static/media/fondB3.78a721ac.png"
+  },
+  {
+    "revision": "6b8eaea96b7e0d9c0999fbc34da164b2",
+    "url": "/pokemaze/static/media/fond.6b8eaea9.png"
+  },
+  {
+    "revision": "a3e81d4178f17227eb1375788089d517",
+    "url": "/pokemaze/static/media/stun.a3e81d41.png"
+  },
+  {
+    "revision": "3a885c881b5f28d72f0b05a235a5a473",
+    "url": "/pokemaze/static/media/confusion.3a885c88.png"
+  },
+  {
+    "revision": "fb6fa4d6bf0404212e08e419dbaf2446",
+    "url": "/pokemaze/static/media/icecube.fb6fa4d6.png"
+  },
+  {
+    "revision": "5624ee2cafb14cf59f41d0e136593de3",
+    "url": "/pokemaze/static/media/ronflex1.5624ee2c.png"
+  },
+  {
+    "revision": "cf80db9217b3af73a0d445478c8df708",
+    "url": "/pokemaze/static/media/star.cf80db92.png"
+  },
+  {
+    "revision": "96d145389fb7d5d55c1177dfd3d8daea",
+    "url": "/pokemaze/index.html"
+  }
+];
